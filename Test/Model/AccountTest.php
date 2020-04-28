f7c8c77d8d58fc82a5612157b5dca796d25072d5
Peter Sjöqvist <info@mageparts.com>
2020-04-28 12:40:54 +0200

    * Fixed comments and testing data.
    M2C-68

--- Test/Model/AccountTest.php
+++ Test/Model/AccountTest.php
@@ -1,18 +1,7 @@
 <?php
 /**
- * Copyright 2016 Resurs Bank AB
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
  */
 
 declare(strict_types=1);
@@ -27,7 +16,7 @@ use PHPUnit\Framework\TestCase;
 /**
  * Test cases designed for Account data model.
  *
- * @package Resursbank\Core\Test\Model\Config\Source
+ * @package Resursbank\Core\Test\Model
  */
 class AccountTest extends TestCase
 {
@@ -104,10 +93,10 @@ class AccountTest extends TestCase
      */
     public function testSalt()
     {
-        $this->account->setSalt('asd56yrrgFgh34fffdDTgh5terfgedfg¤3wdf');
+        $this->account->setSalt('dfgdg456y4rtgrhrth456DFg34tefgH6edfg2DF4');
 
         self::assertSame(
-            'asd56yrrgFgh34fffdDTgh5terfgedfg¤3wdf',
+            'dfgdg456y4rtgrhrth456DFg34tefgH6edfg2DF4',
             $this->account->getSalt()
         );
     }
