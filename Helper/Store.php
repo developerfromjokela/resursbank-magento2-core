cf12e4e10a724099a8ede1f9cb26e62a8eb28602
Peter Sjöqvist <info@mageparts.com>
2020-04-29 09:06:51 +0200

    * Added helper for business logic relating to stores. * Added test class for related business logic.
    M2C-70

---
+++ Helper/Store.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Copyright 2016 Resurs Bank AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Core\Helper;
+
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Provides business logic for handling store specific operations.
+ *
+ * @package Resursbank\Core\Helper
+ */
+class Store extends AbstractHelper
+{
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @param Context $context
+     * @param StoreManagerInterface $storeManager
+     */
+    public function __construct(
+        Context $context,
+        StoreManagerInterface $storeManager
+    ) {
+        $this->storeManager = $storeManager;
+
+        parent::__construct($context);
+    }
+
+    /**
+     * @return int
+     * @throws NoSuchEntityException
+     */
+    public function getActiveStoreId(): int
+    {
+        return (int) $this->storeManager->getStore()->getId();
+    }
+}
