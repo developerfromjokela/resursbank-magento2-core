d37447356114d50c5a587dcac916b30d8081f390
Peter Sjöqvist <info@mageparts.com>
2020-04-30 14:15:36 +0200

    Implementation of business logic to fetch payment methods through EComPHP library.
    ---
    
    * Added relevant unit tests for abstract config service helper.
    * Remove unit test class for store service helper.
    * Minor modifications applied to abstract configuration service helper.
    
    M2C-71

--- Helper/AbstractConfig.php
+++ Helper/AbstractConfig.php
@@ -1,18 +1,7 @@
 <?php
 /**
- * Copyright 2016 Resurs Bank AB
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
  */
 
 declare(strict_types=1);
@@ -66,36 +55,36 @@ abstract class AbstractConfig extends AbstractHelper
      * provided id. This is because the value is read from the compiled cached
      * array, not directly from the database table.
      *
-     * @param string $key
      * @param string $group
+     * @param string $key
      * @param string $scopeType
      * @param null|string $scopeCode
      * @return string
      */
     public function get(
-        string $key,
         string $group,
+        string $key,
         string $scopeType = ScopeInterface::SCOPE_STORE,
         ?string $scopeCode = null
     ) {
         return $this->reader->getValue(
             $this->getPath($group, $key),
             $scopeType,
-            $this->getScope($scopeCode)
+            $scopeCode
         );
     }
 
     /**
-     * @param string $key
      * @param string $group
+     * @param string $key
      * @param string $value
      * @param string $scopeType
      * @param int $scopeId
      * @return void
      */
     public function set(
-        string $key,
         string $group,
+        string $key,
         string $value,
         string $scopeType = ScopeInterface::SCOPE_STORE,
         int $scopeId = 0
@@ -109,13 +98,13 @@ abstract class AbstractConfig extends AbstractHelper
     }
 
     /**
-     * @param string $key
      * @param string $group
+     * @param string $key
      * @return string
      */
-    private function getPath(
-        string $key,
-        string $group
+    protected function getPath(
+        string $group,
+        string $key
     ): string {
         return "resursbank/{$group}/{$key}";
     }
