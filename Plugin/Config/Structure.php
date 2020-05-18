9276c5c270bea0d434a7e968809daf5f71bc7178
Marcus Gullberg <marcus.gullberg@resurs.se>
2020-05-18 13:23:26 +0200

    Plugin to add dynamic methods to config

--- Plugin/Config/Structure.php
+++ Plugin/Config/Structure.php
@@ -25,6 +25,95 @@ class Structure
         Original $subject,
         array $result
     ): array {
+        try {
+            if ($this->hasConfigElement($result)) {
+                $collection = $this->getFakeMethods();
+
+                // Amend array structure for our payment methods.
+                $methods = &$result['other_payment_methods']['children']['resursbank_section']
+                    ['children']['resursbank']['children']['methods']['children']['collection'];
+
+                if (!isset($methods['children']) ||
+                    !is_array($methods['children'])
+                ) {
+                    $methods['children'] = [];
+                }
+
+                foreach ($collection as $method) {
+                    $this->addPaymentMethod($methods, $method);
+                }
+            }
+        } catch (Exception $e) {
+            // Log error
+
+            throw $e;
+        }
+
         return $result;
+    }
+
+    /**
+     * Appends a dynamic payment method to config.
+     *
+     * @param array $config
+     * @param array $method // NEEDS TO CHANGE
+     */
+    private function addPaymentMethod(
+        array &$config,
+        array $method
+    ) {
+        $config['children'][$method['code']] = [
+            'id' => $method['code'],
+            'translate' => 'label',
+            'sortOrder' => 0,
+            'showInDefault' => 1,
+            'showInWebsite' => 1,
+            'showInStore' => 1,
+            'label' => $method['label'],
+            '_elementType' => 'group',
+            'path' => 'payment/resursbank_section/resursbank/methods/collection',
+            'children' => [
+                'sort_order' => [
+                    'id' => 'sort_order',
+                    'translate' => 'label',
+                    'type' => 'text',
+                    'sortOrder' => 1,
+                    'showInDefault' => 1,
+                    'showInWebsite' => 1,
+                    'showInStore' => 1,
+                    'label' => 'Sort Order',
+                    '_elementType' => 'field',
+                    'path' => "payment/resursbank_section/resursbank/methods/collection/{$method['code']}"
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * Check if config array includes element for payment methods.
+     *
+     * @param array $result
+     * @return bool
+     */
+    private function hasConfigElement(array $result): bool
+    {
+        return (
+            isset(
+                $result['other_payment_methods']['children']['resursbank_section']
+                ['children']['resursbank']['children']['methods']['children']['collection']
+            ) &&
+            is_array(
+                $result['other_payment_methods']['children']['resursbank_section']
+                ['children']['resursbank']['children']['methods']['children']['collection']
+            )
+        );
+    }
+
+    private function getFakeMethods(): array
+    {
+        return [
+            ['code' => 'test_method_one', 'label' => 'Test method one'],
+            ['code' => 'test_method_two', 'label' => 'Test method two']
+        ]; 
     }
 }
