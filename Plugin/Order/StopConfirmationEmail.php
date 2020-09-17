d2bc82e04c61bd949caa6db9ddd7df1353100b90
Peter Sjöqvist <info@mageparts.com>
2020-09-17 09:20:21 +0200

    M2C-152
    Prevent order confirmation email from being sent upon order creation.

---
+++ Plugin/Order/StopConfirmationEmail.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Copyright © Resurs Bank AB. All rights reserved.
+ * See LICENSE for license details.
+ */
+
+declare(strict_types=1);
+
+namespace Resursbank\Core\Plugin\Order;
+
+use Exception;
+use Magento\Sales\Model\Order;
+use Resursbank\Rco\Helper\Log;
+
+/**
+ * Prevents the order confirmation email from being sent upon order creation.
+ * The order confirmation email will instead be sent when we receive a callback
+ * indicating the payment has been confirmed by Resurs Bank.
+ *
+ * @package Resursbank\Core\Plugin\Order
+ */
+class StopConfirmationEmail
+{
+    /**
+     * @var Log
+     */
+    private $log;
+
+    /**
+     * @param Log $log
+     */
+    public function __construct(
+        Log $log
+    ) {
+        $this->log = $log;
+    }
+
+    /**
+     * @param Order $subject
+     * @param Order $result
+     * @return Order
+     * @throws Exception
+     */
+    public function afterBeforeSave(
+        Order $subject,
+        Order $result
+    ): Order {
+        try {
+            if ($this->isEnabled($subject)) {
+                $subject->setCanSendNewEmailFlag(false);
+            }
+        } catch (Exception $e) {
+            $this->log->exception($e);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Check whether or not this plugin should execute.
+     *
+     * @param Order $order
+     * @return bool
+     * @throws Exception
+     */
+    private function isEnabled(
+        Order $order
+    ): bool {
+        return (
+            $order->isObjectNew() &&
+            !$order->getOriginalIncrementId() &&
+            $order->getGrandTotal() > 0
+        );
+    }
+}
