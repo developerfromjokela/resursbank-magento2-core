8242fdb321818805d8ef8b9e7b386fbce6f89388
Peter Sjöqvist <info@mageparts.com>
2021-08-19 15:35:45 +0200

    M2C-251
    * Updated documentation in payment adapter.

--- Model/Payment/Resursbank.php
+++ Model/Payment/Resursbank.php
@@ -43,13 +43,6 @@ class Resursbank extends Adapter
      * relay call to the parent constructor. Since the design pattern for the
      * payment method adapters keep changing we should avoid that for now.
      *
-     * NOTE: We need the Resurs Bank method model applied within the adapter
-     * to reach values otherwise handled by configured value handlers. At the
-     * time of writing the payment method instance nor code is made available to
-     * the value handler, thus we cannot extract values associated with our
-     * dynamic methods from their table through the value handlers (so we do
-     * not know what method we should resolve values for).
-     *
      * @param PaymentMethodInterface $model
      */
     public function setResursModel(
