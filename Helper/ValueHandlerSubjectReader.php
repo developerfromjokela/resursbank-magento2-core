cfcb2e4eac07fa023f526c87df393acd309d0039
Mikael Jacobson <mikael.jacobson@resurs.se>
2022-03-24 14:08:04 +0100

    M2C-330 Add extra checks to prevent exception.

--- Helper/ValueHandlerSubjectReader.php
+++ Helper/ValueHandlerSubjectReader.php
@@ -56,9 +56,12 @@ class ValueHandlerSubjectReader extends AbstractHelper
     public function getMethodInstance(
         array $subject
     ): ?MethodInterface {
+
         return (
             isset($subject['payment']) &&
             $subject['payment'] instanceof PaymentDataObject &&
+            $subject['payment']->getPayment() &&
+            $subject['payment']->getPayment()->getMethod() &&
             $subject['payment']->getPayment()->getMethodInstance() instanceof MethodInterface
         ) ? $subject['payment']->getPayment()->getMethodInstance() : null;
     }
