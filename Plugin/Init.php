e1e0022cdf90b82de620444633437aa2617c4e20
Mikael Jacobson <mikael.jacobson@resurs.se>
2023-05-08 14:41:27 +0200

    MMA-16 Initialize logging with Ecom, rename Debug setting to Logging and make the log level setting depend on the logging setting

--- Plugin/Init.php
+++ Plugin/Init.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Copyright © Resurs Bank AB. All rights reserved.
  * See LICENSE for license details.
@@ -8,16 +9,35 @@ declare(strict_types=1);
 
 namespace Resursbank\Core\Plugin;
 
+use Magento\Framework\Exception\FileSystemException;
+use Magento\Framework\Filesystem\DirectoryList;
+use Magento\Framework\Filesystem\Io\File;
+use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface;
 use Resursbank\Core\Helper\Config;
 use Resursbank\Ecom\Config as EcomConfig;
+use Resursbank\Ecom\Lib\Log\FileLogger;
+use Resursbank\Ecom\Lib\Log\LoggerInterface as EcomLoggerInterface;
+use Resursbank\Ecom\Lib\Log\NoneLogger;
+use Throwable;
 
 /**
  * Handles initial init of Ecom+.
  */
 class Init
 {
-    public function __construct(private Config $config)
-    {
+    public function __construct(
+        private Config $config,
+        private DirectoryList $directoryList,
+        private File $file,
+        private LoggerInterface $logger,
+        private StoreManagerInterface $storeManager
+    ) {
+        $logPath = $this->getLogPath();
+
+        if (!is_dir(filename: $logPath)) {
+            $this->file->mkdir(dir: $logPath);
+        }
     }
 
     /**
@@ -25,6 +45,40 @@ class Init
      */
     public function beforeLaunch()
     {
-        EcomConfig::setup();
+        EcomConfig::setup(
+            logger: $this->getLogger(),
+            logLevel: $this->config->getLogLevel(scopeCode: $this->storeManager->getStore()->getCode())
+        );
+    }
+
+    /**
+     * @return string
+     * @throws FileSystemException
+     */
+    private function getLogPath(): string
+    {
+        return $this->directoryList->getPath(code: 'var') . '/log/resursbank';
+    }
+
+    /**
+     * Fetch a logger instance.
+     *
+     * @return EcomLoggerInterface
+     */
+    private function getLogger(): EcomLoggerInterface
+    {
+        $logger = new NoneLogger();
+
+        if (!$this->config->isLoggingEnabled()) {
+            return $logger;
+        }
+
+        try {
+            $logger = new FileLogger(path: $this->getLogPath());
+        } catch (Throwable $error) {
+            $this->logger->error(message: $error->getMessage());
+        }
+
+        return $logger;
     }
 }
