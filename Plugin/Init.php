efd09d14f444ae7b8d59a17e461f7b89094d1cd4
Peter Sjöqvist <info@mageparts.com>
2023-06-07 08:59:32 +0200

    MMA-59
    * Cleaned up unused code.

--- Plugin/Init.php
+++ Plugin/Init.php
@@ -9,36 +9,7 @@ declare(strict_types=1);
 
 namespace Resursbank\Core\Plugin;
 
-use Magento\Framework\App\ProductMetadataInterface;
-use Magento\Framework\App\Cache\StateInterface;
-use Magento\Framework\Exception\FileSystemException;
-use Magento\Framework\Filesystem\DirectoryList;
-use Magento\Framework\Filesystem\Io\File;
-use Magento\Framework\Locale\Resolver as Locale;
-use Psr\Log\LoggerInterface;
-use Resursbank\Core\Helper\Api;
-use Resursbank\Core\Helper\Config;
-use Resursbank\Core\Helper\Log;
 use Resursbank\Core\Helper\Mapi;
-use Resursbank\Core\Helper\Scope;
-use Resursbank\Core\Helper\Version;
-use Resursbank\Core\Model\Cache\Ecom as Cache;
-use Resursbank\Core\Model\Cache\Type\Resursbank;
-use Resursbank\Ecom\Config as EcomConfig;
-use Resursbank\Ecom\Lib\Api\Environment;
-use Resursbank\Ecom\Lib\Api\GrantType;
-use Resursbank\Ecom\Lib\Api\Scope as EcomScope;
-use Resursbank\Ecom\Lib\Cache\CacheInterface;
-use Resursbank\Ecom\Lib\Cache\None;
-use Resursbank\Ecom\Lib\Locale\Language;
-use Resursbank\Ecom\Lib\Log\FileLogger;
-use Resursbank\Ecom\Lib\Log\LoggerInterface as EcomLoggerInterface;
-use Resursbank\Ecom\Lib\Log\NoneLogger;
-use Resursbank\Ecom\Lib\Model\Network\Auth\Jwt;
-use Resursbank\Ecom\Module\Store\Repository;
-use Throwable;
-
-use function is_dir;
 
 /**
  * Handles initial init of Ecom+.
