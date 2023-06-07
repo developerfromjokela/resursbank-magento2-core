c31cce9959d98168fbb0df88dd982db42269d791
Peter Sjöqvist <info@mageparts.com>
2023-06-07 09:38:56 +0200

    MMA-59
    * Code cleanup.

--- view/adminhtml/web/js/system/stores.js
+++ view/adminhtml/web/js/system/stores.js
@@ -9,47 +9,47 @@ require([
     'mage/translate',
     'domReady!'
 ], function ($, alert, $t) {
-    const fetcher = new Resursbank_FetchStores();
+    const fetcher = new Resursbank_FetchStores({
+        getClientIdInputElement: function() {
+            return document.getElementById(
+                'payment_other_resursbank_section_api_client_id_' +
+                fetcher.getSelectEnvironmentElement().value
+            );
+        },
 
-    fetcher.getClientIdInputElement = function() {
-        return document.getElementById(
-            'payment_other_resursbank_section_api_client_id_' +
-            fetcher.getSelectEnvironmentElement().value
-        );
-    };
+        getClientSecretInputElement: function() {
+            return document.getElementById(
+                'payment_other_resursbank_section_api_client_secret_' +
+                fetcher.getSelectEnvironmentElement().value
+            );
+        },
 
-    fetcher.getClientSecretInputElement = function() {
-        return document.getElementById(
-            'payment_other_resursbank_section_api_client_secret_' +
-            fetcher.getSelectEnvironmentElement().value
-        );
-    };
+        errorHandler: function(message) {
+            alert({
+                title: 'Resurs Bank',
+                content: message
+            });
 
-    fetcher.handleError = function(message) {
-        alert({
-            title: 'Resurs Bank',
-            content: message
-        });
+            if (fetcher.fetching) {
+                fetcher.toggleFetch(false);
+            }
+        },
 
-        if (fetcher.fetching) {
-            fetcher.toggleFetch(false);
+        onComplete: function() {
+            alert({
+                title: 'Resurs Bank',
+                content: $t('rb-please-select-store')
+            });
+        },
+
+        onToggle: function(state) {
+            if (state) {
+                $(document).trigger('ajaxSend', [undefined, { showLoader: true }]);
+            } else {
+                $(document).trigger('ajaxComplete', [undefined, { showLoader: true }]);
+            }
         }
-    };
-
-    fetcher.onComplete = function() {
-        alert({
-            title: 'Resurs Bank',
-            content: $t('rb-please-select-store')
-        });
-    };
-
-    fetcher.onToggle = function(state) {
-        if (state) {
-            $(document).trigger('ajaxSend', [undefined, { showLoader: true }]);
-        } else {
-            $(document).trigger('ajaxComplete', [undefined, { showLoader: true }]);
-        }
-    };
+    });
 
     $('#resursbank_fetch_stores_btn').on('click', fetcher.fetchStores.bind(fetcher));
 });
